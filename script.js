4096fa69799ab45cbf3af74fcc56224deda322ab
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-08-17 11:28:14 +1000

    Finish functionality for single operation

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-const oneBtn = document.getElementById("1");
-const twoBtn = document.getElementById("2");
-const threeBtn = document.getElementById("3");
-const fourBtn = document.getElementById("4");
-const fiveBtn = document.getElementById("5");
-const sixBtn = document.getElementById("6");
-const sevenBtn = document.getElementById("7");
-const eightBtn = document.getElementById("8");
-const nineBtn = document.getElementById("9");
-const zeroBtn = document.getElementById("0");
-const multiplyBtn = document.getElementById("*");
-const divideBtn = document.getElementById("/");
-const addBtn = document.getElementById("+");
 const equalsBtn = document.getElementById("=");
 const cancelBtn = document.getElementById("C");
+const screenContainer = document.querySelector(".screen");
+const firstNumPlaceholder = document.querySelector(".first-num-placeholder");
 
 const buttons = document.querySelectorAll(".calculator-button");
 const operationBtns = document.querySelectorAll(".operation-button");
 console.log(operationBtns);
 const numberBtns = document.querySelectorAll(".number-button");
+let displayContent = document.querySelector(".screen-content");
 
 // Two number variables and an operator variable
 let firstNum = "";
 let secondNum = "";
 let operator = "";
 
-let displayContent = document.querySelector(".screen-content");
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// Display content in screen div
-const displayContentFunc = function (key) {
-  displayContent.textContent = key;
+const displayContentFunc = function (number) {
+  displayContent.textContent = number;
 };
+
+// Create placeholder element for first number
+const createPlaceHolder = function () {
+  const placeholder = document.createElement("div");
+  screenContainer.appendChild.placeholder;
+};
+
+createPlaceHolder();
 //////////////////////////////////////////////////////////////////////////////////////
 
 //////////////////////////////////////////////////////////////////////////////////////
@@ -39,10 +36,12 @@ const displayContentFunc = function (key) {
 for (let i = 0; i < numberBtns.length; i++) {
   numberBtns[i].addEventListener("click", function () {
     if (operator.length < 1) {
-      firstNum = Number(firstNum.concat(numberBtns[i].id));
+      firstNum = firstNum.concat(numberBtns[i].id);
+      displayContentFunc(firstNum);
       console.log(firstNum);
     } else if (operator.length == 1) {
-      secondNum = Number(secondNum.concat(numberBtns[i].id));
+      secondNum = secondNum.concat(numberBtns[i].id);
+      displayContentFunc(secondNum);
       console.log(secondNum);
     }
   });
@@ -53,31 +52,26 @@ for (let i = 0; i < operationBtns.length; i++) {
   operationBtns[i].addEventListener("click", function () {
     if (operator.length < 1) {
       operator = operator.concat(operationBtns[i].id);
+      firstNumPlaceholder.textContent = firstNum + " " + operator;
       console.log(operator);
     }
   });
 }
 
-// Push to second array if operator is in its own array
-// let resultsArrayTwo = [];
-// for (let i = 0; i < numberBtns.length; i++) {
-//   numberBtns[i].addEventListener("click", function () {
-//     if (operator.length == 1) {
-//     }
-//   });
-// }
-
 equalsBtn.addEventListener("click", function () {
-  console.log(operate(operator, firstNum, secondNum));
+  firstNumPlaceholder.textContent = "";
+  displayContentFunc(operate(operator, firstNum, secondNum));
   firstNum = "";
   secondNum = "";
   operator = "";
 });
 
 cancelBtn.addEventListener("click", function () {
+  firstNumPlaceholder.textContent = "";
   firstNum = "";
   secondNum = "";
   operator = "";
+  displayContentFunc(firstNum);
 });
 
 //////////////////////////////////////////////////
@@ -106,7 +100,7 @@ const operate = function (op, num1, num2) {
 
 // Basic calculator functions
 const add = function (a, b) {
-  return a + b;
+  return a * 1 + b * 1;
 };
 const subtract = function (a, b) {
   return a - b;
